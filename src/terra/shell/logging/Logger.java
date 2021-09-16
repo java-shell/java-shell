e49a2b343c46a76e3197c5521583b7ae0184e1d7
Dan <danschirripa98@gmail.com>
2021-09-16 16:23:00 -0400

    Added setDebugStream to Logger to allow debug to be redirected

--- src/terra/shell/logging/Logger.java
+++ src/terra/shell/logging/Logger.java
@@ -18,16 +18,15 @@ public class Logger implements Serializable {
 	final private transient String name;
 	final private transient int id;
 	private transient PrintStream out = LogManager.out;
+	private transient PrintStream debug = out;
 	private transient boolean filter = true;
 	private transient boolean useOut = false;
 
 	/**
 	 * Create a Logger with this numerical ID and Display Name
 	 * 
-	 * @param name
-	 *            Display Name
-	 * @param id
-	 *            Numerical ID
+	 * @param name Display Name
+	 * @param id   Numerical ID
 	 */
 	public Logger(String name, int id, boolean useOut) {
 		this.useOut = useOut;
@@ -44,9 +43,8 @@ public class Logger implements Serializable {
 	 * Specify whether or not to use an OutputStream or to use the LogManager.write
 	 * function
 	 * 
-	 * @param useOut
-	 *            Whether or not to use an OutputStream (True) or LogManager.write
-	 *            (False) Default: False
+	 * @param useOut Whether or not to use an OutputStream (True) or
+	 *               LogManager.write (False) Default: False
 	 */
 	public void useOut(boolean useOut) {
 		this.useOut = useOut;
@@ -56,8 +54,7 @@ public class Logger implements Serializable {
 	 * Create a Logger with this numerical ID, the ID will be used as the Display
 	 * Name
 	 * 
-	 * @param id
-	 *            Numerical ID
+	 * @param id Numerical ID
 	 */
 	public Logger(int id, boolean useOut) {
 		this.useOut = useOut;
@@ -66,11 +63,23 @@ public class Logger implements Serializable {
 	}
 
 	/**
+	 * Set the PrintStream for which debug messages should be logged to, if set to
+	 * null use same as default logs
+	 * 
+	 * @param out PrintStream for which debug messages should be written
+	 */
+	public void setDebugStream(PrintStream out) {
+		if (out == null) {
+			this.debug = this.out;
+		}
+		this.debug = out;
+	}
+
+	/**
 	 * Write a String out to the current OutputStream. This String will be checked
 	 * for variables within the String unless otherwise specified.
 	 * 
-	 * @param s
-	 *            String to write.
+	 * @param s String to write.
 	 */
 	public void log(String s) {
 		final String[] tmp = chkVar(s);
@@ -82,8 +91,7 @@ public class Logger implements Serializable {
 	 * Write an array of Strings out to the current OutputStream. This Array will be
 	 * checked for variables within itself unless otherwise specified.
 	 * 
-	 * @param s
-	 *            Array Of String to be written.
+	 * @param s Array Of String to be written.
 	 */
 	public void log(String[] s) {
 		// print("[" + name + "]");
@@ -126,10 +134,8 @@ public class Logger implements Serializable {
 	/**
 	 * Write out to this Stream temporarily. Only one line will be written here
 	 * 
-	 * @param s
-	 *            String to be written.
-	 * @param out
-	 *            PrintStream to write out to.
+	 * @param s   String to be written.
+	 * @param out PrintStream to write out to.
 	 */
 	public void log(String s, PrintStream out) {
 		out.print("[" + name + "] " + s + "\n");
@@ -138,8 +144,7 @@ public class Logger implements Serializable {
 	/**
 	 * Print this String without ending the line, and without any Display Name.
 	 * 
-	 * @param s
-	 *            String to be written.
+	 * @param s String to be written.
 	 */
 	public void print(String s) {
 		if (useOut) {
@@ -152,15 +157,14 @@ public class Logger implements Serializable {
 	/**
 	 * Write an Error, this goes to System.err.
 	 * 
-	 * @param s
-	 *            String to be written.
+	 * @param s String to be written.
 	 */
 	public void err(String s) {
 		System.err.print("[" + name + "] " + s + "\n");
 	}
 
 	public void debug(String s, int level) {
-		debug(s, out);
+		debug(s, debug);
 	}
 
 	public void debug(String s, int level, PrintStream out) {
@@ -171,20 +175,17 @@ public class Logger implements Serializable {
 	/**
 	 * Debug stream, only prints if "debug" is marked true in launch configuration
 	 * 
-	 * @param s
-	 *            String to be written
+	 * @param s String to be written
 	 */
 	public void debug(String s) {
-		debug(s, out);
+		debug(s, debug);
 	}
 
 	/**
 	 * Debug stream, only prints if "debug" is marked true in launch configuration
 	 * 
-	 * @param s
-	 *            String to be written
-	 * @param out
-	 *            Stream to write to
+	 * @param s   String to be written
+	 * @param out Stream to write to
 	 */
 	public void debug(String s, PrintStream out) {
 		if (LogManager.doDebug()) {
@@ -242,8 +243,7 @@ public class Logger implements Serializable {
 	/**
 	 * Change the current OutputStream.
 	 * 
-	 * @param out
-	 *            The OutputStream to be written to.
+	 * @param out The OutputStream to be written to.
 	 */
 	public void setOutputStream(OutputStream out) {
 		this.out = new PrintStream(out);
@@ -253,8 +253,7 @@ public class Logger implements Serializable {
 	 * Replace all variable declarations within a String with their value
 	 * counterparts
 	 * 
-	 * @param in
-	 *            String to be evaluated
+	 * @param in String to be evaluated
 	 * @return An Array of Strings representing the newly formed line including all
 	 *         variable values
 	 */
@@ -278,8 +277,7 @@ public class Logger implements Serializable {
 	 * Replace all variable declarations within a String with their value
 	 * counterparts
 	 * 
-	 * @param args
-	 *            An Array of Strings to be checked for variables
+	 * @param args An Array of Strings to be checked for variables
 	 * @return An Array of Strings representing the newly formed line including all
 	 *         variable values
 	 */
