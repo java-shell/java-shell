5c99f7f029e8674fa507cecf4d4d9a068b310369
Daniel <danschirripa98@gmail.com>
2023-07-11 19:24:23 -0400

    Updated builtins for LuaLogger

--- src/terra/shell/utils/lua/builtin/LuaLogger.java
+++ src/terra/shell/utils/lua/builtin/LuaLogger.java
@@ -1,5 +1,9 @@
 package terra.shell.utils.lua.builtin;
 
+import java.util.function.Consumer;
+
+import com.hk.lua.Lua;
+import com.hk.lua.LuaObject;
 import com.hk.lua.LuaUserdata;
 
 import terra.shell.logging.LogManager;
@@ -7,6 +11,7 @@ import terra.shell.logging.Logger;
 
 public class LuaLogger extends LuaUserdata {
 	private Logger log;
+	private static final LuaObject luaLoggerMetatable = Lua.newTable();
 
 	public LuaLogger(String name) {
 		log = LogManager.getLogger(name);
@@ -14,12 +19,27 @@ public class LuaLogger extends LuaUserdata {
 
 	@Override
 	public String name() {
-		return log.getName();
+		return "logger";
 	}
 
 	@Override
 	public Object getUserdata() {
 		return log;
+	}
+
+	static {
+		luaLoggerMetatable.rawSet("__name", Lua.newString("LOGGER"));
+		luaLoggerMetatable.rawSet("__index", luaLoggerMetatable);
+
+		LuaObject logFunction = Lua.newFunc(new Consumer<LuaObject[]>() {
+
+			@Override
+			public void accept(LuaObject[] t) {
+			}
+
+		});
+
+		luaLoggerMetatable.rawSet("log", logFunction);
 	}
 
 }
